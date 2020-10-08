9c96360b3ab8e48eb0bbafc0c1ba4658797c8631
Samuel Ryberg <samuel.ryberg@gmail.com>
2020-10-08 19:46:53 +0200

    Update Core.h

--- Snow/src/Snow/Core.h
+++ Snow/src/Snow/Core.h
@@ -5,7 +5,7 @@
 		#define SNOW_API __declspec(dllexport)
 	#else
 		#define SNOW_API __declspec(dllimport)
-	#endif // HZ_BUILD_DLL
+	#endif // SNOW_BUILD_DLL
 #else
 	#error Snow only supports Windows!
-#endif // HZ_PLATFORM_WINDOWS
+#endif // SNOW_PLATFORM_WINDOWS
